a93187769956b56b81c729053ed9cdd92f7b41a1
kadimitr <kanio.dimitrov@microsoft.com>
2017-07-27 19:18:53 +0900

    Adding methods for getting meeting suggestions and scheduling a meeting.

--- SampleAADv2Bot/Dialogs/RootDialog.cs
+++ SampleAADv2Bot/Dialogs/RootDialog.cs
@@ -1,23 +1,26 @@
 ﻿using BotAuth.AADv2;
 using BotAuth.Dialogs;
 using BotAuth.Models;
-using BotAuth;
 using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Connector;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
-using System.Linq;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Web;
+using Microsoft.Graph;
+using Newtonsoft.Json;
+using System.Text;
+using SampleAADv2Bot.Services;
 
 namespace SampleAADv2Bot.Dialogs
 {
     [Serializable]
     public class RootDialog : IDialog<string>
     {
+        MeetingService meetingService = new MeetingService();
+
         public async Task StartAsync(IDialogContext context)
         {
             context.Wait(MessageReceivedAsync);
@@ -26,24 +29,159 @@ namespace SampleAADv2Bot.Dialogs
         public virtual async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> item)
         {
             var message = await item;
+            // Get meeting suggestions
+            await GetMeetingSuggestions(context, message);
+            // Schedule a meeting
+            //await ScheduleMeeitng(context, message);
 
-            // Initialize AuthenticationOptions and forward to AuthDialog for token
-            AuthenticationOptions options = new AuthenticationOptions()
+        }
+
+        private AuthenticationOptions GetAuthenticationOptions()
+        {
+            return new AuthenticationOptions()
             {
                 Authority = ConfigurationManager.AppSettings["aad:Authority"],
                 ClientId = ConfigurationManager.AppSettings["aad:ClientId"],
                 ClientSecret = ConfigurationManager.AppSettings["aad:ClientSecret"],
-                Scopes = new string[] { "User.Read" },
+                // TBD - Read the scopres from Web.Config 
+                Scopes = new string[] { "User.Read", "Calendars.ReadWrite", "Calendars.ReadWrite.Shared" },
                 RedirectUrl = ConfigurationManager.AppSettings["aad:Callback"]
             };
-            await context.Forward(new AuthDialog(new MSALAuthProvider(), options), async (IDialogContext authContext, IAwaitable<AuthResult> authResult) =>
-            {
-                var result = await authResult;
-
-                // Use token to call into service
-                var json = await new HttpClient().GetWithAuthAsync(result.AccessToken, "https://graph.microsoft.com/v1.0/me");
-                await authContext.PostAsync($"I'm a simple bot that doesn't do much, but I know your name is {json.Value<string>("displayName")} and your UPN is {json.Value<string>("userPrincipalName")}.But expect a lot more from me shortly!");
-            }, message, CancellationToken.None);
         }
+
+        // TBD - inject function logic for the interaction with Graph API 
+        private async Task GetMeetingSuggestions(IDialogContext context, IMessageActivity message)
+        {
+            try
+            {
+                await context.Forward(new AuthDialog(new MSALAuthProvider(), GetAuthenticationOptions()), async (IDialogContext authContext, IAwaitable<AuthResult> authResult) =>
+                {
+                    #region TBD Replace with real input
+                    var userFindMeetingTimesRequestBody = new UserFindMeetingTimesRequestBody()
+                    {
+                        Attendees = new List<Attendee>()
+                    {
+                        new Attendee()
+                        {
+                            EmailAddress = new EmailAddress(){
+                                Address ="gled4er@microsoft312.onmicrosoft.com",
+                                Name ="Alex Darrow" }
+                        }
+                    },
+                        LocationConstraint = new LocationConstraint()
+                        {
+                            IsRequired = false,
+                            SuggestLocation = true,
+                            Locations = new List<LocationConstraintItem>()
+                        {
+                            new LocationConstraintItem()
+                            {
+                                DisplayName = "Conf Room 32/1368",
+                                LocationEmailAddress = "conf32room1368@imgeek.onmicrosoft.com"
+                            }
+                        }
+                        },
+                        TimeConstraint = new TimeConstraint()
+                        {
+                            Timeslots = new List<TimeSlot>()
+                        {
+                            new TimeSlot()
+                            {
+                                Start = new DateTimeTimeZone()
+                                {
+                                    DateTime = "2017-07-27T04:19:50.442Z",
+                                    TimeZone = "Pacific Standard Time"
+                                },
+                                End = new DateTimeTimeZone()
+                                {
+                                    DateTime = "2017-08-03T04:19:50.442Z",
+                                    TimeZone = "Pacific Standard Time"
+                                }
+                            }
+                        }
+                        },
+                        MeetingDuration = new Duration(new TimeSpan(2, 0, 0)),
+                        MaxCandidates = 15,
+                        IsOrganizerOptional = false,
+                        ReturnSuggestionReasons = true,
+                        MinimumAttendeePercentage = 100
+
+                    };
+                    #endregion
+                    var result = await authResult;
+                    var meetingTimeSuggestion = await meetingService.GetMeetingsTimeSuggestions(result.AccessToken, userFindMeetingTimesRequestBody);
+                    var stringBuilder = new StringBuilder();
+                    foreach (var suggestion in meetingTimeSuggestion.MeetingTimeSuggestions)
+                    {
+                        stringBuilder.AppendLine($"start - {suggestion.MeetingTimeSlot.Start.DateTime.ToString()} and end - {suggestion.MeetingTimeSlot.End.DateTime.ToString()}");
+                    }
+                    await authContext.PostAsync($"Thesre are the options for meeting {stringBuilder.ToString()}");
+                }, message, CancellationToken.None);
+            }
+            catch(Exception ex)
+            {
+                var msg = ex.Message;
+                throw ex;
+            }
+            
+        }
+
+        public async Task ScheduleMeeitng(IDialogContext context, IMessageActivity message)
+        {
+            try
+            {
+               
+                await context.Forward(new AuthDialog(new MSALAuthProvider(), GetAuthenticationOptions()), async (IDialogContext authContext, IAwaitable<AuthResult> authResult) =>
+                {
+                    #region TBD Replace with real input 
+                    var meeting = new Event()
+                    {
+                        Subject = "My Event",
+                        Body = new ItemBody()
+                        {
+                            ContentType = BodyType.Html,
+                            Content = "Does late morning work for you?"
+                        },
+                        Start = new DateTimeTimeZone()
+                        {
+                            DateTime = "2017-07-29T07:30:00.000Z",
+                            TimeZone = "UTC"
+                        },
+                        End = new DateTimeTimeZone()
+                        {
+                            DateTime = "2017-07-29T08:30:00.000Z",
+                            TimeZone = "UTC"
+                        },
+                        Location = new Location()
+                        {
+                            DisplayName = "Harry's Bar"
+                        },
+                        Attendees = new List<Attendee>()
+                    {
+                        new Attendee()
+                        {
+                            EmailAddress =  new EmailAddress()
+                            {
+                                Address = "test1@Microsoft312.onmicrosoft.com",
+                               Name = "Test1 User"
+                            },
+                            Type = AttendeeType.Required
+                        },
+                    }
+                    };
+                    #endregion
+                    var result = await authResult;
+                    var scheduledMeeting =  await meetingService.ScheduleMeeting(result.AccessToken, meeting);
+                    await authContext.PostAsync($"Meeting with iCalUId - {scheduledMeeting.ICalUId} is scheduled.");
+                }, message, CancellationToken.None);
+            }
+            catch (Exception ex)
+            {
+                var msg = ex.Message;
+                throw ex;
+            }
+        }
+
+
     }
 }
